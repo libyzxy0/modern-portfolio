22e5f661691056744aa677765559ce248b5a70f4
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2024-04-11 13:22:31 +0800

    fix: remove url redirect

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-//Redirect to main domain
-if (window.location.hostname !== 'libyzxy0.com'  && !window.location.hostname.startsWith('localhost')) {
-    window.location.href = 'https://libyzxy0.com';
-}
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
