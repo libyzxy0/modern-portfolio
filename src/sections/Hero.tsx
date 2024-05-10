7a367c45637b54ac35a4df325f367dd5896d5623
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2024-05-11 07:46:27 +0800

    update hero buttons & add resume

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -16,7 +16,7 @@ export default function Hero() {
         <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight md:text-7xl text-white mt-1 md:mx-20">
           Experienced{" "}
           <b className="text-transparent bg-clip-text bg-gradient-to-r from-sky-200 to-blue-400">
-            full-stack developer
+            full-stack web developer
           </b>{" "}
           with a focus on{" "}
           <b className="text-transparent bg-clip-text bg-gradient-to-r from-sky-200 to-blue-400">
@@ -53,7 +53,7 @@ export default function Hero() {
             <span className="h-8 border-r-[1px] border-dashed border-light-gray-secondary dark:border-dark-gray-secondary"></span>
             <li className="flex flex-col sm:flex-row items-center gap-2">
               <span className="flex items-center font-bold bg-[#0001] dark:bg-[#fff1] py-1 px-3 rounded-xl">
-                1k
+                5k
               </span>
               <span className="text-[0.71rem] sm:text-[0.81rem] leading-[1] text-center sm:text-left">
                 Cups of coffee
@@ -64,11 +64,11 @@ export default function Hero() {
           </ul>
         </div>
         <div className="text-center w-full mt-7 md:mt-10 ">
-          <Button className="px-5 bg-white text-gray-900 text-md rounded-full mx-3 font-mono hover:bg-white hover:opacity-90 md:scale-150 md:mx-14">
+          <Button onClick={() => window.location.href = 'https://libyzxy0.com/Jan_Liby_Dela_Costa_Developer_Resume.pdf' } className="px-5 bg-white text-gray-900 text-md rounded-full mx-3 font-mono hover:bg-white hover:opacity-90 md:scale-150 md:mx-14">
             <IoDocumentTextOutline className="mr-1 h-4 w-4" />
             Resume
           </Button>
-          <Button className="px-5 bg-gray-900 text-white text-md rounded-full mx-3 font-mono md:scale-150 md:mx-14">
+          <Button onClick={() => window.location.href = '#contact'} className="px-5 bg-gray-900 text-white text-md rounded-full mx-3 font-mono md:scale-150 md:mx-14">
             <IoChatbubbleEllipsesOutline className="mr-1 h-4 w-4" />
             Contact
           </Button>
